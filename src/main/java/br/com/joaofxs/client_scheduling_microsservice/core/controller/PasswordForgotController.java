6f154ddcb5de2d448ce17d42bf6ae960e3465f68
João Felix <83048381+JoaoFXs@users.noreply.github.com>
2025-12-31 00:13:18 -0300

    feat: Criação de PasswordForgotController

---
+++ src/main/java/br/com/joaofxs/client_scheduling_microsservice/core/controller/PasswordForgotController.java
@@ -0,0 +1,27 @@
+package br.com.joaofxs.client_scheduling_microsservice.core.controller;
+
+
+import br.com.joaofxs.client_scheduling_microsservice.core.dto.RequestResetDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/forgot-password")
+public class PasswordForgotController {
+
+    @Autowired
+    private PasswordForgotService service;
+
+    @PostMapping
+    public ResponseEntity<?> requestReset(@RequestBody RequestResetDTO requestResetDTO){
+
+        service.processPasswordReset(requestResetDTO.email());
+
+        return ResponseEntity.ok("If the email is registered, you'll get a reset link");
+    }
+
+}
---
+++ src/main/java/br/com/joaofxs/client_scheduling_microsservice/core/service/PasswordForgotService.java
@@ -0,0 +1,10 @@
+package br.com.joaofxs.client_scheduling_microsservice.core.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PasswordForgotService {
+
+
+}
